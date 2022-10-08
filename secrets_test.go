b03d58194a9611bb269484a83fde2969cd315bd1
Seth Hoenig <shoenig@duck.com>
2022-10-08 10:45:41 -0500

    build: update repo, use gha, update go, etc

--- secrets_test.go
+++ secrets_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/shoenig/test/must"
 )
 
 func TestText_New(t *testing.T) {
@@ -12,7 +12,7 @@ func TestText_New(t *testing.T) {
 
 	text := New("abc123")
 	value := text.Secret()
-	require.Equal(t, "abc123", value)
+	must.Eq(t, "abc123", value)
 }
 
 func TestText_String(t *testing.T) {
@@ -20,7 +20,7 @@ func TestText_String(t *testing.T) {
 
 	text := New("abc123")
 	s := fmt.Sprintf("%s", text)
-	require.Equal(t, redactString, s)
+	must.Eq(t, redactString, s)
 }
 
 func TestText_GoString(t *testing.T) {
@@ -28,7 +28,7 @@ func TestText_GoString(t *testing.T) {
 
 	text := New("abc123")
 	s := fmt.Sprintf("%#v", text)
-	require.Equal(t, redactTextGoString, s)
+	must.Eq(t, redactTextGoString, s)
 }
 
 func TestText_Equal(t *testing.T) {
@@ -38,14 +38,14 @@ func TestText_Equal(t *testing.T) {
 		a := New("foo")
 		b := New("foo")
 		same := a.Equal(b)
-		require.True(t, same)
+		must.True(t, same)
 	})
 
 	t.Run("different", func(t *testing.T) {
 		foo := New("foo")
 		bar := New("bar")
 		same := foo.Equal(bar)
-		require.False(t, same)
+		must.False(t, same)
 	})
 }
 
@@ -55,12 +55,12 @@ func TestBytes_NewBytes(t *testing.T) {
 	original := []byte{1, 2}
 	bs := NewBytes(original)
 	value1 := bs.Secret()
-	require.Equal(t, []byte{1, 2}, value1)
+	must.Eq(t, []byte{1, 2}, value1)
 
 	// modify original, bs must not change
 	original[0] = 9
 	value2 := bs.Secret()
-	require.Equal(t, []byte{1, 2}, value2)
+	must.Eq(t, []byte{1, 2}, value2)
 }
 
 func TestBytes_String(t *testing.T) {
@@ -68,7 +68,7 @@ func TestBytes_String(t *testing.T) {
 
 	bs := NewBytes([]byte{1, 2, 3})
 	s := fmt.Sprintf("%s", bs)
-	require.Equal(t, redactString, s)
+	must.Eq(t, redactString, s)
 }
 
 func TestBytes_GoString(t *testing.T) {
@@ -76,7 +76,7 @@ func TestBytes_GoString(t *testing.T) {
 
 	bs := NewBytes([]byte{1, 2, 3})
 	s := fmt.Sprintf("%#v", bs)
-	require.Equal(t, redactBytesGoString, s)
+	must.Eq(t, redactBytesGoString, s)
 }
 
 func TestBytes_Equal(t *testing.T) {
@@ -86,13 +86,13 @@ func TestBytes_Equal(t *testing.T) {
 		a := NewBytes([]byte("foo"))
 		b := NewBytes([]byte("foo"))
 		same := a.Equal(b)
-		require.True(t, same)
+		must.True(t, same)
 	})
 
 	t.Run("different", func(t *testing.T) {
 		foo := NewBytes([]byte("foo"))
 		bar := NewBytes([]byte("bar"))
 		same := foo.Equal(bar)
-		require.False(t, same)
+		must.False(t, same)
 	})
 }
