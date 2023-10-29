49c103467ce016f12b273a87a17cec157f04674d
Seth Hoenig <shoenig@duck.com>
2023-10-29 14:08:13 -0500

    json: marshal should use actual text (#17)

--- encoders.go
+++ encoders.go
@@ -3,7 +3,7 @@ package conceal
 // MarshalText implements the encoding.TextMarshaller interface, which is also
 // compatible with the encoding/json marshaller.
 func (t *Text) MarshalText() ([]byte, error) {
-	return []byte(t.String()), nil
+	return []byte(t.Unveil()), nil
 }
 
 // UnmarshalText implements the encoding.TextUnmarshaler interface, which is
